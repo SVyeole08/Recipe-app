8befe4ed056723b2dea133a29db0cb5f199de2e1
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-19 17:01:06 +0530

    enhance the toastify pop up

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,34 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Recipecontext from "./Context/Recipecontext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Recipecontext>
     <BrowserRouter>
       <App />
-      <ToastContainer position="top-center" />
+      <ToastContainer
+        position="top-center"
+        autoClose={2200}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Slide}
+        className="mt-2"
+        toastClassName={() =>
+          "relative flex p-4 min-h-[3.5rem] rounded-xl justify-between overflow-hidden cursor-pointer bg-white/10 backdrop-blur border border-white/20 text-white shadow-xl"
+        }
+        bodyClassName={() => "text-sm leading-relaxed"}
+        progressClassName="bg-blue-500"
+        closeButton
+      />
     </BrowserRouter>
   </Recipecontext>
 );
